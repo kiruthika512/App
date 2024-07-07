704b6dc20baf15cfd1847db741482e6f73349371
kiruthika512 <prakasamkiruthika@gmail.com>
2024-07-07 21:12:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+import WeatherComponent from './Components/WeatherComponent/WeatherComponent'
+const App = () => {
+  return (
+    <React.Fragment>
+      <WeatherComponent/>
+    </React.Fragment>
+  )
+}
+
+export default App
